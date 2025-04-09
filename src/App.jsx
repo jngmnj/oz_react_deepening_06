41765c53adb7c90e921ca4ad1d3a19471d2da3f4
jngmnj <exorbitance@naver.com>
2025-04-09 19:03:15 +0900

    ✨ React router 실습과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/admin/Home';
 import Layout from './pages/Layout';
+import Home from './pages/admin/Home';
 import Dashboard from './pages/admin__dashboard/Dashboard';
-import Users from './pages/admin__users/Users';
-import UserDetail from './pages/admin__users/UserDetail';
-import Settings from './pages/admin__settings/Settings';
 import NotFound from './pages/admin__notfound/NotFound';
+import Settings from './pages/admin__settings/Settings';
+import UsersDetail from './pages/admin__users/UserDetail';
+import Users from './pages/admin__users/Users';
 
 function App() {
   //1. React Router의 `BrowserRouter`를 설정해야 합니다.
@@ -16,7 +17,20 @@ function App() {
   //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
   //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
 
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="users/:id" element={<UsersDetail />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
